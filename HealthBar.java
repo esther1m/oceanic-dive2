9c1b72f7a7a9ef53e04ab1053814c619630e0e51
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-27 17:02:39 +0000

    healthbar for shipwreck wood

---
+++ HealthBar.java
@@ -0,0 +1,56 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HealthBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HealthBar extends Actor
+{
+    /**
+     * Act - do whatever the HealthBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int health = 50;
+    public HealthBar()
+    {
+        setImage(new GreenfootImage(52, 12));
+        getImage().drawRect(0,0,51,11);
+        getImage().setColor(Color.GREEN);
+        getImage().fillRect(1,1,health,10);
+        
+    }
+    public void act()
+    {
+        setImage(new GreenfootImage(52, 12));
+        getImage().drawRect(0,0,51,11);
+        getImage().setColor(Color.GREEN);
+        getImage().fillRect(1,1,health,10);
+        World world = getWorld();
+        MyWorld myWorld = new MyWorld();
+        setLocation(myWorld.diver.getX() - 5, myWorld.diver.getY() - 50);
+        loseHealth();
+    }
+    public void loseHealth() {
+        World world = getWorld();
+        MyWorld myWorld = new MyWorld();
+        if (myWorld.diver.hitByshipwreckWood())
+        {
+            health--;
+        }
+        if(health<=0)
+        {
+            getWorld().showText("Game Over! \n You survived for " + (myWorld.getDiver().time/60) + " seconds", getWorld().getWidth()/2 , getWorld().getHeight()/2);
+            Greenfoot.stop();
+        }
+    }
+
+    public boolean hitByshipwreckWood() {
+        if (isTouching(ShipwreckWood.class) == true) {
+            return true;
+        } else {
+           return false;
+        }
+    }
+}
