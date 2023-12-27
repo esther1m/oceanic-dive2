ac72d54813d13643f07492c3d2aa5773901a9cd4
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-27 16:16:11 +0000

    seaweed

--- MyWorld.java
+++ MyWorld.java
@@ -8,26 +8,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    //initialising the amount of shipwrecks to be repeated and the amount of seaweed to be in the game
     private static final int NUMBER_OF_SHIPWRECK = 7;
+    private static final int NUMBER_OF_SEAWEED = 10;
+    Diver diver = new Diver();
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
-    {    
-        
+    {            
         
         // Create a new world with 1000x600 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
-        //populating the world with pieces of wood
         populate();
     }
         
     public void populate(){
-        addObject(new Diver(), 500 ,556);
+
+        addObject(diver, 500 ,556);
+
         for (int i = 0; i < NUMBER_OF_SHIPWRECK; i++){
             int x = Greenfoot.getRandomNumber(getWidth());
             addObject(new shipwreckWood(), x, 4);
+        }
+
+        for (int count = 0; count < NUMBER_OF_SEAWEED; count++){
+            int randomX = Greenfoot.getRandomNumber(getWidth());
+            int randomY = (Greenfoot.getRandomNumber(99) + 500);
+            addObject(new seaweed(), randomX, randomY);
         }
 
     }
