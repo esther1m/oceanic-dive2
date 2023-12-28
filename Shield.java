c70da0a2109debde843aac2394501276d94c0f11
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-28 22:51:53 +0000

    shield capabilities powerup

--- Shield.java
+++ Shield.java
@@ -13,64 +13,40 @@ public class Shield extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     //int health = 50;
-    private boolean canLoseHealth = true;
-    private int cooldown = 10;
+    int time = 0;
+    boolean checker = false;
 
     MyWorld myWorld;
     Diver diver3;
     HealthBar health;
-    KrabbyPatty krabbyPatty;
+   
 
     
     public Shield(MyWorld world) {
         myWorld = world;        
         diver3 = myWorld.getDiver();
         health = myWorld.getHealthBar();
+
         
     }
-/* 
     public void act() {
+        time++;
         updateDiverPosition();
-        if (diver2 != null){
-            setLocation(diver2.getX() - 5, diver2.getY() - 50);
-            checkWoodCollision();
+        if (diver3 != null){
+            setLocation(diver3.getX(), diver3.getY());
+            //checkWoodCollision();
         }
-    }
+        health.canLoseHealth = false;
+        health.cooldown = 150;
 
-    private void checkWoodCollision(){
-        if (canLoseHealth && hitByshipwreckWood(diver2)){
-            loseHealth();
-            canLoseHealth = false;
-        } else if (! canLoseHealth) {
-            cooldown--;
-            if (cooldown <= 0) {
-                cooldown = 30;
-                canLoseHealth = true;
-            }
+        if (time/60 >= 45){
+            time =0;
+            myWorld.foodCount = 0;
+            checker = true;
         }
-    }
-
-    public void loseHealth() {
-        if (hitByshipwreckWood(diver2) == true) {
-            health--;
-            updateHealthBar();
-        }
-        if(health<=0) {
-            myWorld.showText("Game Over! \n You survived for " + (myWorld.time/60) + " seconds", getWorld().getWidth()/2 , getWorld().getHeight()/2);
-            Greenfoot.stop();
-        }
-    }
-
-
-
-
-    public boolean hitByshipwreckWood(Actor actor) {
-
-        return isTouching(ShipwreckWood.class);
     }
 
     public void updateDiverPosition(){
-        diver2 = myWorld.getDiver();
+        diver3 = myWorld.getDiver();
     }
 }
-*/}
