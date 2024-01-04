2b47bf84ce442803f4a991e8fd2b6fc18f3db18f
esther1m <146996363+esther1m@users.noreply.github.com>
2024-01-04 02:59:06 +0000

    udated intro screens with better info

---
+++ Level1instruction.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Level1instruction here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Level1instruction extends World
+{
+
+    /**
+     * Constructor for objects of class Level1instruction.
+     * 
+     */
+    public Level1instruction()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+        act();
+    }
+
+    public void act() {
+        showText("Click anywhere to START LEVEL 1 ", 500 , 571);
+        if (Greenfoot.mouseClicked(this) == true) {
+            Greenfoot.setWorld(new Level1());
+        }
+    }
+}
