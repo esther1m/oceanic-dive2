c05e2489a1c1e597561dda0eee22369375fe06cc
esther1m <146996363+esther1m@users.noreply.github.com>
2024-01-05 02:37:47 +0000

    improving code quality, changing class method name

---
+++ Counter.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * @author (Muhammad Fazil) 
+ * @version (v1.0)
+ */
+
+//this class creates a physical counter which keeps track of score, energy, time
+public class Counter extends Actor
+{
+    int score; 
+    int energy;
+    int time;
+    public Counter()
+    {
+        //using built in methods on Greenfoot to create and show the image coded below
+        setImage(new GreenfootImage("Score: " + score + "\n Energy: " + energy + "\n Time: " + time/60, 40, Color.WHITE, new Color (0,0,0,0))); 
+    }
+
+    public void act()
+    {
+        time++;
+        // added so that the counter will update as the game continues
+        setImage(new GreenfootImage("Score: " + score + "\n Energy: " + energy + "\n Time: " + time/60, 40, Color.WHITE, new Color (0,0,0,0)));  
+        youWin();
+    }
+    //
+    public void youWin()
+    {
+        /* if the time in seconds is equal to 180 the code will excectue,
+        text will appear on screen with instructions for the gamer to follow, 
+        there is a greenfoot delay so that the game is essentially paused,
+        */ 
+        if (time/60 == 180)
+        {
+            getWorld().showText("Final score: " + score + "\n \n Press the Z to see what is next... ", getWorld().getWidth()/2 , getWorld().getHeight()/2);
+            Greenfoot.delay(100);
+        }
+        /*if the mouse is pressed and the time is greater than or equal to 
+        then the code will execute and take you to the win screen*/
+        if (Greenfoot.isKeyDown("Z") && time/60 >= 180)
+        {
+            Greenfoot.setWorld(new YouWinScreen());
+        }
+    }
+}
---
+++ LevelTwo.java
@@ -0,0 +1,86 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * @author (Muhammad Fazil) 
+ * @version (v1.0)
+ */
+
+public class LevelTwo extends World
+{
+    int count = 0;
+    // The spawn speed is faster when spawn speed value is lowered
+    int spawnSpeed = 55;
+    int randomSpawn;
+    
+    //making mainPlayer an instance of the new diver so a paramter can be passed
+    public DiverShooter mainPlayer = new DiverShooter(); 
+    //creating an instance of Counter
+    Counter counter = new Counter();  
+    HealthBar2 healthBar = new HealthBar2();
+    GunButton gunButton = new GunButton(counter);
+    // creating an instance of power up so it can be accessed within Level Two
+    PowerUp powerUp = new PowerUp(); 
+    
+    public LevelTwo()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+        // Resets the speed as the speed was faster in level
+        Greenfoot.setSpeed(50);
+        mainPlayer = new DiverShooter(gunButton, powerUp);
+        addObject(mainPlayer, getWidth()/2, getHeight()/2);
+        //sets position of the visual counter on the game
+        addObject(counter, 120, 100); 
+        // adds the health bar above the diver 
+        addObject(healthBar, mainPlayer.getX() - 5, mainPlayer.getY() - 50);
+        // adds the gun upgrade button on the game screen
+        addObject(gunButton, 900, 100); 
+        // adds the power up bar above the diver and health bar 
+        addObject(powerUp, mainPlayer.getX() + 10, mainPlayer.getY() - 80); 
+    }
+    
+    //using return to be able to access the mainPlayer information from World in other classes like the HealthBar and Counter classes
+    public DiverShooter getDiverShooter()
+    {
+        return mainPlayer;
+    }
+
+    public void act()
+    {
+        // incrementing count so creatures can spawn continuously
+        count++; 
+        //the conditional if statement ensures that the spawn speed of creatures gets faster every 10 seconds by decreasing the spawn speed value by 1
+        if (count % 600 == 0)
+        {
+            spawnSpeed--;
+        }
+        // the while loop ensures that if P is down the game will be delayed and paused 
+        while(Greenfoot.isKeyDown("P"))
+        {
+            Greenfoot.delay(1);
+        }
+        spawnCreatures(); 
+    }
+    public void spawnCreatures()
+    {
+        if (count % spawnSpeed == 0)
+        {
+            // setting creatures to spawn to another random spawn location using the random number generated
+            randomSpawn = Greenfoot.getRandomNumber(8);  
+            /* using a switch statement to ensure there are 8 spawn loactions 
+             * passing the mainPlayer so that each creature can recieve the information of the diver
+             * passing the counter so it is visible to the creatures */
+            switch(randomSpawn) 
+            {
+                case 0: addObject(new Creature(mainPlayer,counter), 0, 0);break; 
+                case 1: addObject(new Creature(mainPlayer,counter), getWidth()/2,0); break;
+                case 2: addObject(new Creature(mainPlayer,counter), getWidth(), 0); break;
+                case 3: addObject(new Creature(mainPlayer,counter), 0, getHeight()/2);break;
+                case 4: addObject(new Creature(mainPlayer,counter), getWidth(),getHeight()/2); break;
+                case 5: addObject(new Creature(mainPlayer,counter), 0, getHeight()); break;
+                case 6: addObject(new Creature(mainPlayer,counter), getWidth()/2, getHeight()); break;
+                case 7: addObject(new Creature(mainPlayer,counter), getWidth(), getHeight()); break;
+            }
+        }
+    }
+}
