e47cbc5fe894dc1fafb64399dd4abca6d477c025
esther1m <esthermikhael23@gmail.com>
2024-01-08 00:53:30 +0000

    added comments

--- PlayButton.java
+++ PlayButton.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class PlayButton here.
+ * playbutton functionality to start the game
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (ESTHER MIKHAEL) 
+ * @version (v1.0)
  */
 public class PlayButton extends Actor
 {
@@ -14,7 +14,7 @@ public class PlayButton extends Actor
      */
     public void act() {
         
-        // Add your action code here.
+        // when the play button image is clicked on, it will take you to the storyline screen
         if (Greenfoot.mouseClicked(this) == true) {
             Greenfoot.setWorld(new Storyline());
         } 
