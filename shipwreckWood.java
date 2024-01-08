12aea15956ef4ad2680b3b28c9427fdac97c0616
esther1m <esthermikhael23@gmail.com>
2024-01-08 01:08:05 +0000

    comments added

--- shipwreckWood.java
+++ shipwreckWood.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class shipwreckWood here.
+ * Controls how the shipwrecked wood behaves
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -12,23 +12,31 @@ public class ShipwreckWood extends Actor
      * Act - do whatever the shipwreckWood wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    /* repeatedly checks if there has been a collision with itself so that they don't spawn in a cluster
+     * moves the shipwrecked wood
+     * sets a rotation because otherwise it looks weird
+     * checks the edge so that they respawn at the top of the screen once they've reached the bottom.
+     */
     public void act()
     {
-// coding the wood so that it spawns at the start at a good angle and doesn't spawn on top of another piece of wood
         checkCollision();
         move(1);
         setRotation(90);
         checkEdge();
         
     }
-
+    /* this method checks the edge so that they respawn at the top of the screen once they've reached the bottom.
+     * uses a random x co-ordinate but the same y
+     */
     private void checkEdge(){
         if (isAtEdge()){
             int randX = Greenfoot.getRandomNumber(getWorld().getWidth());
             setLocation(randX, 4);
         }
     }
-
+/* checks collision with itself
+ * if true, then the objects are moved to a new area where they are not colliding
+ */
     private void checkCollision(){
         if (isTouching(getClass()) == true){
             int randX2 = Greenfoot.getRandomNumber(getWorld().getWidth());
