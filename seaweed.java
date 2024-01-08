c8efe84d86d9c181abf29558d66a4644e43dd9b8
esther1m <esthermikhael23@gmail.com>
2024-01-08 00:59:16 +0000

    comments

--- seaweed.java
+++ seaweed.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class seaweed here.
+ * Makes the seaweed turn one way and the other every now and again
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (OLIVIA FIELDING) 
+ * @version (v1.0)
  */
 public class Seaweed extends Actor
 {
@@ -12,26 +12,18 @@ public class Seaweed extends Actor
      * Act - do whatever the seaweed wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    //initial direction the seaweed will turn in
     int direction = -1;
+    
+    //allows the seaweed to turn around 45 degrees before switching direction
+    //if the rotation is 0 or the opposite way, it will change direction again by multiplying the direction variable by -1
     public void act()
     {
-
-        //move(1);
-        //turn(5);
-
-
         turn(6 * direction);
         if (getRotation() >= 45 || getRotation() <= -45) {
             direction *= -1;
-            //turn(3 * direction);
         } else if (getRotation() == 0 || getRotation() < -45) {
             direction *= -1;
         }
-        
-   
-
-        
-
-        // Add your action code here.
     }
 }
